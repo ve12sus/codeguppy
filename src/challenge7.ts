733a7d20ba21fd9f5225f835628b0c34d1073f5b
jeff <jeff@ve12sus.com>
2021-02-09 17:16:36 -0500

    finished challenge 7

--- src/challenge7.ts
+++ src/challenge7.ts
@@ -1,8 +1,13 @@
 // calculate the sum of odd numbers greater than 10 and less or equal than 30
 
-for ( let i = 11; i <= 30; i ++ ) {
-    if ( i % 2 != 0 ) {
-        i += i;
-    }
-    console.log(i);
+function sumOfOdd() {
+    let sum = 0;
+
+    for ( let i = 11; i <= 30; i += 2 ) {
+        sum += i;
+    };
+
+    console.log(sum);
 };
+
+sumOfOdd();

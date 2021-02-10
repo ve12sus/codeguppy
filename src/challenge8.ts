e54a065121162eb57ccdea2f69f7dfb8ce8886e5
jeff <jeff@ve12sus.com>
2021-02-09 21:35:38 -0500

    added challenge8

---
+++ src/challenge8.ts
@@ -0,0 +1,9 @@
+// create a function that will convert from Celcius to Fahrenheit
+// Reminder: C = F - 32 / 1.8
+
+function celciusToFahrenheit(n : number) {
+    return n * 1.8 + 32;
+};
+
+let r = celciusToFahrenheit(20);
+console.log (r);

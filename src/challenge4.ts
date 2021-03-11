c7417a6996f0a83b031e3fdeefd877d0d82ec14a
jeff <jeff@ve12sus.com>
2021-03-11 18:41:38 -0500

    challenge 4 tests

--- src/challenge4.ts
+++ src/challenge4.ts
@@ -1,13 +1,20 @@
 // print all the multiplication tables with numbers from 1 to 10
 
-for ( let i = 0; i <= 10; i++ ) {
-    printTable(i);
-    console.log("");
-};
-
 function printTable(n : number) {
-    for ( let i = 0; i <= 10; i++ ) {
-        let row = n + " times " + i + " = " + n * i;
-        console.log(row);
-    }
-};
+  const rows : string[] = [];
+  for (let i = 0; i <= 10; i += 1) {
+    const row = `${n} times ${i} equals ${n * i}`;
+    // n + " times " + i + " = " + n * i;
+    rows.push(row);
+  }
+  return rows;
+}
+
+export function multiplyTable(n : number) : string[] {
+  let table : string[] = [];
+  for (let i = 0; i <= n; i += 1) {
+    const oneRow = printTable(i);
+    table = table.concat(oneRow);
+  }
+  return table;
+}
---
+++ test/challenge4.test.ts
@@ -0,0 +1,11 @@
+// print all the multiplication tables with numbers from 1 to 10
+import { expect } from 'chai'
+import { multiplyTable } from '../src/challenge4'
+
+describe('print all the multiplication tables with numbers from 1 to 10', function() {
+  describe('#multiplyTable()', function() {
+    it('should print all the multiplication tables with numbers from 1 to 10', function() {
+      expect(multiplyTable(10).length).to.deep.equal(121);
+    });
+  });
+});

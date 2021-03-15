ab429f5929a84c927ee003825ef7ea92db12ae7d
jeff <jeff@ve12sus.com>
2021-03-15 14:58:03 -0400

    challenge 12b test (learned arrow func)

--- src/challenge12b.ts
+++ src/challenge12b.ts
@@ -1,15 +1,14 @@
 // Create a function that receives an array of numbers and returns an array
 // containing only the positive numbers.
-// Requirement: Use a “for” loop
+// Requirement: Use Array.prototype.forEach()
 
 export function getPositives(ar : number[]) : number[] {
   const ar2 : number[] = [];
 
-  for (let i = 0; i < ar.length; i += 1) {
-    const el = ar[i];
+  ar.forEach(el => () => {
     if (el >= 0) {
       ar2.push(el);
     }
-  }
+  });
   return ar2;
 }
---
+++ test/challenge12b.test.ts
@@ -0,0 +1,14 @@
+// Create a function that receives an array of numbers and returns an array 
+// containing only the positive numbers.
+// Requirement: Use Array.prototype.forEach()
+
+import { expect } from 'chai'
+import { getPositives } from '../src/challenge12a'
+
+describe('receives an array of numbers and returns an array containing only the positive numbers', function() {
+  describe('#getPositives()', function() {
+    it('should return an array containing only the positive numbers', function() {
+      expect(getPositives([-1,2,-3])).to.deep.equal([2]);
+    });
+  });
+});

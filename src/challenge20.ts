a30fc616b08be64ae3cdf6ab0115a2a74a0d91d9
jeff <jeff@ve12sus.com>
2021-03-16 23:08:54 -0400

    removed console.logs

--- src/challenge20.ts
+++ src/challenge20.ts
@@ -2,11 +2,8 @@
 
 export function rotateLeft(ar : number[]) : number[] {
   let first = 0;
-  console.log(ar);
   if (ar.length > 1) {
-    console.log(ar);
     first = ar.shift() as number;
-    console.log(first);
     ar.push(first);
   }
   return ar;

754780fde6c733f6c1de94e89352ba750d5cea95
jeff <jeff@ve12sus.com>
2021-03-17 17:12:37 -0400

    challenge 30 test

--- src/challenge30a.ts
+++ src/challenge30a.ts
@@ -1,3 +1,31 @@
 // Create a function that will add two indefinite size numbers.
 // Requirements: Only positive numbers will be used and will be provided to
 // the function as strings. The result should be also returned as a string
+
+function digitFromRight(s : string, digitNo : number) {
+  if (digitNo >= s.length) return 0;
+  const char = s[s.length - 1 - digitNo];
+  return parseInt(char, 10);
+}
+
+export function add(sNumber1 : string, sNumber2 : string) : string {
+  let s = '';
+  let carry = 0;
+
+  const maxSize = Math.max(sNumber1.length, sNumber2.length);
+
+  for (let i = 0; i < maxSize; i += 1) {
+    const digit1 = digitFromRight(sNumber1, i);
+    const digit2 = digitFromRight(sNumber2, i);
+
+    const sum = digit1 + digit2 + carry;
+    const digitSum = sum % 10;
+    carry = sum >= 10 ? 1 : 0;
+
+    s = digitSum.toString() + s;
+  }
+
+  if (carry > 0) s = carry.toString() + s;
+
+  return s;
+}
--- test/challenge30a.test.ts
+++ test/challenge30a.test.ts
@@ -1,3 +1,17 @@
 // Create a function that will add two indefinite size numbers.
 // Requirements: Only positive numbers will be used and will be provided to
 // the function as strings. The result should be also returned as a string
+
+import { expect } from 'chai'
+import { add } from '../src/challenge30a'
+
+const n1 = '2909034221912398942349';
+const n2 = '129092390909309499';
+
+describe('Add two indefinite size numbers', function() {
+  describe('#add()', function() {
+    it('should add two indefinite size numbers', function() {
+      expect(add(n1, n2)).to.deep.equal('2909163314303308251848');
+    });
+  });
+});
